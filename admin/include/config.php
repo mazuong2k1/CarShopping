fd02c0d4f7388419581a031377c566741a5d629b
Nguyen Vu Quang <a1tp.quang@gmail.com>
2023-07-15 03:16:13 +0700

    make it better

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'shopping');
+define('DB_SERVER','us-cdbr-east-06.cleardb.net');
+define('DB_USER','b624e40d471799');
+define('DB_PASS' ,'0102c216');
+define('DB_NAME', 'heroku_68cb7c3d29ad637');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
